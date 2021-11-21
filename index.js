4cb38c529f16ccff4efa14532382e834e55f44b1
novalmarsel32 <novalmarsel32@gmail.com>
2021-11-21 23:28:59 +0700

    testing nopal

---
+++ index.js
@@ -0,0 +1,6 @@
+const nama = "Noval Gimank";
+let hobi = "Playing Game";
+let pekerjaan = "Jual Akun Game";
+let makanan = "Ayam Goreng";
+console.log(`${nama} ${hobi} ${pekerjaan}`)
+console.log(`${makanan}`)
